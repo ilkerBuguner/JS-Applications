9181284570d1682f117c5e19e6d071a6260b496a
İlker Bugüner <ilker.buguner@gmail.com>
2020-07-05 00:15:43 +0300

    Solved one more homework problem from REST Services and AJAX lecture.

--- 02-REST Services and AJAX/Homework/03.PHONEBOOK/app.js
+++ 02-REST Services and AJAX/Homework/03.PHONEBOOK/app.js
@@ -1,5 +1,39 @@
 function attachEvents() {
-    console.log('TODO...');
+    const elements = {
+        person() {return document.getElementById('person')},
+        phone() {return document.getElementById('phone')},
+        createContact() {return document.getElementById('btnCreate')},
+        phonebook() {return document.getElementById('phonebook')},
+        loadContacts() {return document.getElementById('btnLoad')}
+    };
+    const contacts = [];
+    const baseUrl = 'http://localhost:3000/contacts';
+    elements.createContact().addEventListener('click', () => {
+        const {value: person} = elements.person();
+        const {value: phone} = elements.phone();
+
+        fetch(baseUrl, {
+            method: "POST",
+            body: JSON.stringify({person, phone})
+        })
+            .then((response) => response.json())
+            .then((response) => {
+                contacts.push(response);
+                elements.person().value = '';
+                elements.phone().value = '';
+            })
+
+    });
+
+    elements.loadContacts().addEventListener('click', () => {
+        elements.phonebook().textContent = '';
+        contacts.forEach((contact) => {
+            let li = document.createElement('li');
+            const key = Object.keys(contact)[0];
+            li.textContent = `${contact[key].person} - ${contact[key].phone}`;
+            elements.phonebook().appendChild(li);
+        });
+    });
 }
 
 attachEvents();
