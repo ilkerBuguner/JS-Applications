f91c600355f3c762e73c52ce01070bc180d2730c
İlker Bugüner <ilker.buguner@gmail.com>
2020-07-05 22:31:24 +0300

    Solved all homework problems from REST Services and AJAX lecture.

--- 02-REST Services and AJAX/Homework/04.MESSENGER/app.js
+++ 02-REST Services and AJAX/Homework/04.MESSENGER/app.js
@@ -1,5 +1,40 @@
 function attachEvents() {
-    console.log('TODO...');
+    const elements = {
+        author() {return document.getElementById('author')},
+        content() {return document.getElementById('content')},
+        submitBtn() {return document.getElementById('submit')},
+        refreshBtn() {return document.getElementById('refresh')},
+        messagesField() {return document.getElementById('messages')}
+    };
+
+    const baseUrl = 'http://localhost:3000/messenger';
+    const chat = [];
+
+    elements.submitBtn().addEventListener('click', () => {
+        const authorName = elements.author().value;
+        const messageContent = elements.content().value;
+
+        fetch(baseUrl, {
+            method: "POST",
+            body: JSON.stringify({authorName, messageContent})
+        })
+            .then((response) => response.json())
+            .then((response) => {
+                chat.push(response);
+                elements.author().value = '';
+                elements.content().value = '';
+            })
+    });
+
+    elements.refreshBtn().addEventListener('click', () => {
+        elements.messagesField().value = '';
+        chat.forEach((chatMessage) => {
+            const key = Object.keys(chatMessage)[0];
+            const contentToPush = `\n${chatMessage[key].authorName} - ${chatMessage[key].messageContent}`;
+            elements.messagesField().value += contentToPush;
+        });
+    });
+    
 }
 
 attachEvents();
