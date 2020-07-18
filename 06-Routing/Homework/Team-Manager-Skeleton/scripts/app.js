bea315e2418b53f699b9a90abc77b14e7c46d05b
İlker Bugüner <ilker.buguner@gmail.com>
2020-07-18 23:37:01 +0300

    Solved a part from exercise problem.

--- 06-Routing/Homework/Team-Manager-Skeleton/scripts/app.js
+++ 06-Routing/Homework/Team-Manager-Skeleton/scripts/app.js
@@ -1,1 +1,29 @@
- // TODO
+import home from './controllers/home.js';
+import about from './controllers/about.js';
+import login from './controllers/login.js';
+import register from './controllers/register.js';
+import catalog from './controllers/catalog.js';
+
+$(() => {
+    const app = Sammy('#main', function() {
+        this.use('Handlebars', 'hbs');
+        this.userData = {
+            loggedIn: true,
+            hasTeam: false
+        };
+
+        this.get('index.html', home);
+        this.get('#/home', home);
+        this.get('/', home);
+
+        this.get('#/about', about);
+
+        this.get('#/register', register);
+
+        this.get('#/login', login);
+
+        this.get('#/catalog', catalog);
+    });
+
+    app.run();
+});
